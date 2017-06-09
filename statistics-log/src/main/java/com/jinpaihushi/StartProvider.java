99c39ee862e6051b302a8180d691bb960871aa2e
120591516 <120591516@qq.com>
2017-06-09 18:28:24 +0800

    a

---
+++ statistics-log/src/main/java/com/jinpaihushi/StartProvider.java
@@ -0,0 +1,9 @@
+package com.jinpaihushi;
+
+import com.jinpaihushi.parse.ParseLog;
+
+public class StartProvider {
+    public static void main(String[] args) {
+        ParseLog.readFileByLines();
+    }
+}
