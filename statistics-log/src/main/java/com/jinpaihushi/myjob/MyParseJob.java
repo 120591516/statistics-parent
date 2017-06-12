f074f7e9df08b3c580832cfb410f4f518ab4e542
120591516 <120591516@qq.com>
2017-06-12 10:59:36 +0800

    a

--- statistics-log/src/main/java/com/jinpaihushi/myjob/MyParseJob.java
+++ statistics-log/src/main/java/com/jinpaihushi/myjob/MyParseJob.java
@@ -2,18 +2,12 @@ package com.jinpaihushi.myjob;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jinpaihushi.parse.Parse114Log;
 import com.jinpaihushi.parse.ParseWxLog;
 import com.jinpaihushi.parse.ParseYykLog;
 
-@Controller
-@RequestMapping("/myJob")
-public class MyParseJobController {
+public class MyParseJob {
     private Logger logger = Logger.getLogger(getClass());
 
     @Autowired
@@ -25,9 +19,7 @@ public class MyParseJobController {
     @Autowired
     private ParseYykLog parseYykLog;
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    @ResponseBody
-    public String parseLogJob() {
+    public void parseLogJob() {
         logger.info("开始解析微信公众号日志");
         parseWxLog.readFileByLines();
         logger.info("解析微信公众号日志结束");
@@ -37,6 +29,5 @@ public class MyParseJobController {
         logger.info("开始医养康日志");
         parseYykLog.readFileByLines();
         logger.info("解析医养康结束");
-        return "YES";
     }
 }
